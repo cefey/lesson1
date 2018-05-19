f5807b2722b7b0e938491b0a110c765e71dea433
desperado321 <staffa261@gmail.com>
2018-05-19 15:51:24 +0300

    Lesson 1

--- area_triangle.rb
+++
@@ -1,6 +0,0 @@
-puts "       Area of ​​a triangle"
-puts "Input Base of a triangle"
-base=Integer (gets.chomp)
-puts "Input height of a triangle"
-height=Integer (gets.chomp)
-puts " Area of a tringle = #{(base*height)/2}"
